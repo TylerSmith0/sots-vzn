0c82e5a0949f92ced5a75713e4bda59b6f79bba4
Tyler Smith <smithtylern@gmail.com>
2022-04-27 21:27:13 -0400

    changed debug mode to True

--- ss/settings.py
+++ ss/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8x!ex1y=66pft!hkrve0xownn+^66rf#wif5+gu2!y=bju(_zh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://saviorsofthesea.com']
 
 
 # Application definition
