360997370fd385fa8218394fea515fdf27af5706
Tyler Smith <smithtylern@gmail.com>
2022-05-20 21:30:27 -0400

    fsdkjl

--- ss/settings.py
+++ ss/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 try:
     dotenv.load_dotenv("./.env")
