edc9a3eba070dcef8ce3039237f43045f30aa2b7
Tyler Smith <smithtylern@gmail.com>
2022-05-29 16:23:02 -0600

    adjusted static location

--- ss/settings.py
+++ ss/settings.py
@@ -158,8 +158,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'ss/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'ss')
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'pages')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'pages')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
